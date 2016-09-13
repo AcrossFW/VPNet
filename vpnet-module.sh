354102f66147af514611b65134f7f836061465c9
AcrossFW <dev@acrossfw.com>
2016-09-13 19:46:14 +0000

    rename local var name in get_user_home

--- vpnet-module.sh
+++ vpnet-module.sh
@@ -14,7 +14,7 @@ vpnet::init_bash() {
   set -o pipefail
   set -o nounset
   # set -o xtrace
-  
+
   local source=$1
   if [[ -z "$source" ]]; then
     echo "ERROR: vpnet::init_bash must have BASH_SOURCE[0] as arg1"
@@ -24,8 +24,9 @@ vpnet::init_bash() {
   # Set magic variables for current file & dir
   __dir="$(cd "$(dirname "$source}")" && pwd)"
   __file="${__dir}/$(basename "${source}")"
-  __base="$(basename ${__file} .sh)"
+  __base="$(basename "${__file}" .sh)"
   __root="$(cd "$(dirname "${__dir}")" && pwd)" # <-- change this as it depends on your app
+  __SC2034="$__base $__root" && __SC2034=$__SC2034
 }
 
 # [Templating with Linux in a Shell Script](http://serverfault.com/a/699377/276381)
@@ -47,9 +48,9 @@ vpnet::init_config() {
     exit -1
   }
   
-  echo "initing $config_file from $template_file ..."
-  template="$(cat ${template_file})"
-  eval "echo \"${template}\"" > $config_file
+  echo "vpnet::init_config initing $config_file from $template_file ..."
+  template="$(cat "${template_file}")"
+  eval "echo \"${template}\"" > "$config_file"
 }
  
 vpnet::is_docker() {
@@ -80,7 +81,7 @@ vpnet::init_system() {
   echo "Setting hostname to $HOSTNAME ..."
   # XXX: this will not work in --net=host mode
   # https://github.com/docker/docker/issues/5708
-  hostname $HOSTNAME
+  hostname "$HOSTNAME"
   
   echo "Disabling coredump ..."
   sysctl fs.suid_dumpable=0 
@@ -109,22 +110,22 @@ vpnet::init_network() {
 
 vpnet::get_user_home() {
   local n=${#@}
-  local user_name
-  local user_home
+  local __user_name
+  local __user_home
   local __resultvar
   
   case "$n" in
     1)
-      user_name=$1
-      user_home=$(eval echo ~${user_name})
-      echo $user_home
+      __user_name=$1
+      __user_home=$(eval echo ~"${__user_name}")
+      echo "$__user_home"
       ;;
     2)  # http://www.linuxjournal.com/content/return-values-bash-functions
-      user_name=$1
+      __user_name=$1
       __resultvar=$2
-      user_home=$(eval echo ~${user_name})
-      # echo $__resultvar="'$user_home'"
-      eval $__resultvar="'$user_home'"
+      __user_home=$(eval echo ~"${__user_name}")
+      # echo $__resultvar="'$__user_home'"
+      eval "$__resultvar='$__user_home'"
       ;;
     *)
       return -1
@@ -132,47 +133,10 @@ vpnet::get_user_home() {
   esac
   
   # non-exist user will not resolve and keep the origin string, which has a leading '~'
-  if [[ "$user_home" =~ ^~ ]]; then
+  if [[ "$__user_home" =~ ^~ ]]; then
     return -1
   else
     return 0
   fi
 }
 
-#
-# Process Args
-#
-vpnet::run() {
-  local arg1=$1
-  case "$arg1" in
-    start)
-      # Use baseimage-docker's init system.
-      # CMD ["/sbin/my_init"]
-      echo -n "Getting my IP... "
-      curl -sS ifconfig.io
-      
-      exec my_init
-      
-      err_code=$?
-      echo "ERROR: my_init exit with error code $err_code"
-      sleep 1
-      exit $err_code
-      ;;
-    test)
-      echo "test ok!"
-      exit $?
-      ;;
-    bash|sh|shell)
-      echo "Creating shell..."
-      exec /bin/bash -s
-      exit $?
-      ;;
-    *)
-      echo "ERROR: Unsupport arg $arg1"
-      exit -1
-      ;;
-  esac
-  
-  echo "ERROR: Should not run to here!"
-  exit -1
-}
