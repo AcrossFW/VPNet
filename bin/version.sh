d632f280d3cce619861eef89afee0974625ed29f
AcrossFW <dev@acrossfw.com>
2016-09-14 10:43:32 +0000

    add version.sh cli

---
+++ bin/version.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#
+# VPNet.io - Virtual Private Network Essential Toolbox
+#
+# https://github.com/acrossfw/vpnet
+#
+# CLI version.sh
+#
+source "/acrossfw/vpnet-functions.sh"
+vpnet::init_bash "${BASH_SOURCE[0]}" # set all the magic
+
+main() {
+  local arg1=${1:-}
+
+hash=$(head -1 .git/logs/HEAD | awk '{print $2}')
+echo ${hash:0:7}
+}
+
+main "$@"
+
