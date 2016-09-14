9c9179664a5c9af64380a4ebfcaa92110959b20c
AcrossFW <dev@acrossfw.com>
2016-09-14 08:36:50 +0000

    functionilize code

--- script/build.sh
+++ script/build.sh
@@ -1,108 +1,199 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # VPNet.io - Virtual Private Network Essential Toolbox
 #
 # https://github.com/acrossfw/vpnet
 #
-set -o errexit
-set -o pipefail
-set -o nounset
+# Build Tool
+#
+source /acrossfw/vpnet-functions.sh
+vpnet::init_bash "${BASH_SOURCE[0]}" # set all the magic
 
-docker_image="vpnet"
-net_mode=bridge
-
-arg1=${1:-build}
-
-case "$arg1" in
-	build|'')
-		docker build -t $docker_image .
-		err_code=$?
-		if [ "$err_code" = 0 ]; then
-			echo
-			echo
-			echo
-			echo '############################################'
-			echo "Build succeed for docker_image $docker_image"
-			echo '############################################'
-			echo
-			echo
-			echo
-			exit
-		else
-			echo
-			echo
-			echo
-			echo '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
-			echo "ERROR: Build FAIL with exit code $err_code"
-			echo '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
-			echo
-			echo
-			echo
-		fi
-		
-		exit $err_code
-		;;
-
-	run)
-		# http://stackoverflow.com/a/1655389/1123955
-		read -r docker_run<<-CMD
-			docker run -ti --rm --privileged --net=$net_mode \
-	  		-p 2222:22 \
-	  		-p 1723:1723 \
-	  		-p 3128:3128 \
-	  		-p 8388:8388 \
-	  		$docker_image
-CMD
-		echo
-		echo "$docker_run"
-		
-		exec "$docker_run"
-		
-		echo "ERROR: exec docker fail with error code $"
-		exit -1
-		;;
-
-	exec)
-		container_id=$(docker ps | grep vpnet | awk '{print $1}' | head -1)
-		docker_exec="docker exec -ti $container_id /bin/bash"
-		
-		echo
-		echo "$docker_exec"
-		echo
-		
-		exec "$docker_exec"
-		
-		echo "ERROR: exec docker fail: $?"
-		exit -1
-		;;
-		
-	ssh)
-		[ "$net_mode" = "host" ] 		&& ssh_port=2222	&& ssh_host=localhost
-		[ "$net_mode" = "bridge" ]	&& ssh_port=22		&& ssh_host=$(docker ps | grep vpnet | awk '{print $1}' | xargs docker inspect | grep IPAddress | grep 172 | awk -F\" '{print $4}' | head -1)
-		
-		echo "SSHing to $ssh_host:$ssh_port in $net_mode mode ... "
-		read -r ssh_run<<-CMD
-			ssh -p $ssh_port -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@${ssh_host}
-CMD
-		echo
-		echo "$ssh_run"
-		echo
-		exec "$ssh_run"
-		echo "ERROR: exec ssh fail with error code $?"
-		exit -1
-		;;
+main() {
+	local arg1=${1:-build}
 	
-	lint)
-		file_list=$(find . -type f -name "*.sh" -o -name "run")
-		for file in $file_list; do
-			shellcheck --exclude SC2093,SC2078 "$file" || ret_code=$?
-			bash -n "$file" || ret_code=$?
-		done
-		exit $ret_code
-		;;
+	local docker_image='vpnet'
+	local net_mode='bridge'
+
+	case "$arg1" in
+		build|'')
+			builder::build "$docker_image"						|| echo "ERROR: builder::build FAIL"
+			;;
+	
+		run)
+			builder::run "$docker_image" "$net_mode"	|| echo "ERROR: builder::run FAIL"
+			;;
+	
+		exec)
+			builder::exec "$docker_image" 						|| echo "ERROR: builder::exec FAIL"
+			;;
+			
+		ssh)
+			builder::ssh "$docker_image" "$net_mode"	|| echo "ERROR: builder::ssh FAIL"
+			;;
 		
-	*)
-		exec docker run --name vpnet -ti --rm --privileged $docker_image "$@"
-		echo "ERROR: exec docker fail with error code $?"
-		exit -1
-esac
+		test)
+			builder::test $docker_image								|| echo "ERROR: builder::test FAIL"
+			;;
+
+		lint)
+			builder::lint 														|| echo "ERROR: builder::lint FAIL"
+			;;
+		
+		*)
+			docker_run_cmd="docker run --name vpnet -ti --rm --privileged $docker_image $*"
+
+			echo
+			echo "$docker_run_cmd"
+			echo
+			
+			$docker_run_cmd 													|| echo "ERROR: docker run fail with error code $?"
+			;;
+	esac
+	
+	exit
+}
+
+builder::build() {
+	local docker_image=$1
+	
+	docker build -t "$docker_image" . || {
+		echo
+		echo
+		echo
+		echo '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
+		echo "ERROR: Build FAIL with exit code $err_code"
+		echo '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
+		echo
+		echo
+		echo
+		return -1
+	}
+	
+	echo
+	echo
+	echo
+	echo '############################################'
+	echo "Build succeed for docker_image $docker_image"
+	echo '############################################'
+	echo
+	echo
+	echo
+
+	return 0
+}
+
+builder::run() {
+	local docker_image=$1
+	local net_mode=$2
+	
+	# http://stackoverflow.com/a/1655389/1123955
+	read -r docker_run<<-CMD
+		docker run -ti --rm --privileged --net=$net_mode \
+  		-p 2222:22 \
+  		-p 1723:1723 \
+  		-p 3128:3128 \
+  		-p 8388:8388 \
+  		$docker_image
+CMD
+	echo
+	echo "$docker_run"
+	
+	$docker_run || {
+		echo "ERROR: docker run fail with error code $?"
+		return -1
+	}
+	
+	return 0
+}
+
+builder::exec() {
+	local docker_image=$1
+	
+	local container_id
+	
+	container_id=$(docker ps | grep vpnet | awk '{print $1}' | head -1)
+	docker_exec="docker exec -ti $container_id /bin/bash"
+	
+	echo
+	echo "$docker_exec"
+	echo
+	
+	$docker_exec || {
+		echo "ERROR: docker exec fail: $?"
+		return -1
+	}
+	return 0
+}
+
+builder::test() {
+	local docker_image=$1
+	
+	local docker_run_cmd="docker run -ti --rm --privileged $docker_image test"
+	
+	echo
+	echo "$docker_run_cmd"
+	echo
+	
+	$docker_run_cmd || {
+		echo "ERROR: docker run test fail with error code $?"
+		return -1
+	}
+	
+	return 0
+}
+
+builder::ssh() {
+	local docker_image=$1
+	local net_mode=$2
+	
+	local ssh_host
+	local ssh_port
+	
+	case "$net_mode" in
+		host)
+			ssh_host='localhost'
+			ssh_port=2222
+			;;
+		bridge|*)
+			ssh_host=$(docker ps | grep vpnet | awk '{print $1}' | xargs docker inspect | grep IPAddress | grep 172 | awk -F\" '{print $4}' | head -1)
+			ssh_port=22
+			;;
+	esac
+	
+	echo "SSHing to $ssh_host:$ssh_port in $net_mode mode ... "
+	ssh_cmd="ssh -p $ssh_port -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@${ssh_host}"
+
+	echo
+	echo "$ssh_cmd"
+	echo
+	
+	$ssh_cmd || {
+		echo "ERROR: exec ssh fail with error code $?"
+		return -1
+	}
+	return 0
+}
+
+builder::lint() {
+	local ret_code
+	local file_list
+	local file
+	
+	ret_code=0
+	file_list=$(find . -type f -name "*.sh" -o -name "run")
+	for file in $file_list; do
+		shellcheck --exclude SC2093,SC2078 "$file" || ret_code=$?
+		bash -n "$file" || ret_code=$?
+	done
+	
+	if [[ "$ret_code" = 0 ]]; then
+		echo
+		echo "Linting PASS"
+		echo
+	fi
+	
+	return $ret_code
+}
+
+main "$@"
